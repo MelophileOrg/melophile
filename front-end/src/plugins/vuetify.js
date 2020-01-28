7a7eb13bcd4e4df446a4985222a595a4bc30c603
Andrew Young <andrew@youngshome.com>
2020-01-28 12:05:52 -0700

    Transfer

--- front-end/src/plugins/vuetify.js
+++ front-end/src/plugins/vuetify.js
@@ -1,4 +1,3 @@
-import '@fortawesome/fontawesome-free/css/all.css'
 import Vue from 'vue';
 import Vuetify from 'vuetify/lib';
 
@@ -14,8 +13,5 @@ export default new Vuetify({
           error: '#b71c1c',
         }
       }
-    },
-    icons: {
-      iconfont: 'fa',
     },
 });
