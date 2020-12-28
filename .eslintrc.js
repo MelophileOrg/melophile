9b0eec819430c0f36eeab242e258fcdaae86a395
Andrew Young <andrew@youngshome.com>
2020-12-27 23:58:49 -0800

    Inicial Structure

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,18 @@
 module.exports = {
   root: true,
   env: {
-    node: true,
+    browser: true,
+    node: true
+  },
+  parserOptions: {
+    parser: 'babel-eslint'
   },
   extends: [
-    'plugin:vue/essential',
-    '@vue/airbnb',
+    '@nuxtjs',
+    'plugin:nuxt/recommended'
   ],
-  parserOptions: {
-    parser: 'babel-eslint',
-  },
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'import/prefer-default-export': 'off',
-    'max-len': 'off',
-    'import/named': 'off',
-    'import/extensions': 'off',
-    'consistent-return': 'off',
-    'import/no-cycle': 'off',
-    camelcase: 'off',
-  },
-};
+  plugins: [
+  ],
+  // add your custom rules here
+  rules: {}
+}
--- src/api/request.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-const request = axios.create({
-  baseURL: '/api',
-});
-
-export default request;
